b3316c4ae3d6f140a365aded6d749932e50b65c9
David Jin <david-jin@scas.nsw.edu.au>
2019-10-17 20:15:21 +1100

    Added SwipeCellKit

--- Water Trax/Controller/HousesTableViewController.swift
+++ Water Trax/Controller/HousesTableViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 import SwipeCellKit
 
-class HousesTableViewController: UITableViewController,{
+class HousesTableViewController: UITableViewController{
     
      var houseNameArray = ["House1", "Apartment1"]
      var houseAddressDict = ["House1" : "39 Parkland Avanue", "Apartment1" : "18 George Street"]
-    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(houseAddressDict)
         print(houseNameArray[0])
         self.tableView.backgroundView = UIImageView(image: UIImage(named: "adrien-olichon-_VZ2MBS7OvU-unsplash"))
+        self.tableView.rowHeight = 85.0
 
 
         // Uncomment the following line to preserve selection between presentations
@@ -42,6 +43,7 @@ class HousesTableViewController: UITableViewController,{
         
         
         let houseName = houseNameArray[indexPath.row]
+        print(indexPath.row)
         cell.textLabel?.text = houseName
         cell.textLabel?.textColor = .systemTeal
         cell.textLabel?.font = .systemFont(ofSize: 25)
@@ -72,11 +74,9 @@ class HousesTableViewController: UITableViewController,{
                UITextField.self.keyboardAppearance = .dark
                 
                 AddHouseAlert.addTextField { (UITextField) in
-                UITextField.self.placeholder = "Address"
+                UITextField.self.placeholder = "Address - Optional"
                 UITextField.self.keyboardType = .default
                 UITextField.self.keyboardAppearance = .dark
-                 
-                
            }
 
            let continueAction = UIAlertAction(title: "Continue", style: .default) { [unowned AddHouseAlert] _ in
@@ -87,7 +87,7 @@ class HousesTableViewController: UITableViewController,{
             self.houseAddressDict[String(name.text!)] = String(address.text!)
             
             self.tableView.reloadData()
-             
+            
              
            }
            let cancelAction = UIAlertAction(title: "Cancel", style: .default) { [] _ in
@@ -103,4 +103,86 @@ class HousesTableViewController: UITableViewController,{
     
 }
 }
-extension 
+extension HousesTableViewController : SwipeTableViewCellDelegate {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            print("Deleted")
+            let house = self.houseNameArray[indexPath.row]
+            self.houseAddressDict.removeValue(forKey: String(house))
+            self.houseNameArray.remove(at: indexPath.row)
+            print(self.houseNameArray)
+            print(self.houseAddressDict)
+        }
+        let editAction = SwipeAction(style: .default, title: "Edit") { action, indexPath in
+            // handle action by updating model with deletion
+            let AddHouseAlert = UIAlertController(title: "Edit", message: "Edit your house name and address", preferredStyle: .alert)
+                      
+                        AddHouseAlert.addTextField { (UITextField) in
+                        UITextField.self.placeholder = "House Name"
+                        UITextField.self.keyboardType = .default
+                        UITextField.self.keyboardAppearance = .dark
+                           
+                        AddHouseAlert.addTextField { (UITextField) in
+                        UITextField.self.placeholder = "Address"
+                        UITextField.self.keyboardType = .default
+                        UITextField.self.keyboardAppearance = .dark
+                      }
+
+                      let continueAction = UIAlertAction(title: "Save", style: .default) { [unowned AddHouseAlert] _ in
+                       let name = AddHouseAlert.textFields![0]
+                       let address = AddHouseAlert.textFields![1]
+                       let house = self.houseNameArray[indexPath.row]
+                        print(name.text!)
+                       
+                        
+                        if name.text != "" {
+                       self.houseNameArray[indexPath.row] = name.text!
+                       self.tableView.reloadData()
+                        }
+                        if address.text != ""  {
+                            self.houseAddressDict[String(name.text!)] = String(address.text!)
+                            self.houseAddressDict.removeValue(forKey: String(house))
+                            
+                            self.tableView.reloadData()
+                            }
+                            else {
+                            self.houseAddressDict[name.text!] = self.houseAddressDict[house]
+                            self.houseAddressDict.removeValue(forKey: String(house))
+                            
+                            
+                            }
+
+                        
+                        
+                      }
+                      let cancelAction = UIAlertAction(title: "Cancel", style: .default) { [] _ in
+
+                      }
+                          
+                      AddHouseAlert.addAction(cancelAction)
+                      AddHouseAlert.addAction(continueAction)
+                       self.present(AddHouseAlert, animated: true)
+                  }
+               
+               
+            print("Edited")
+        
+            
+        }
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "Trash-Icon")
+        editAction.image = UIImage(named: "Edit-Icon")
+        
+
+        return [deleteAction, editAction]
+    }
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
+}
